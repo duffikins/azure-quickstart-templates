0984e114e2df4fdf2314ee1ea3657019643ed18b
duffikins <42541535+duffikins@users.noreply.github.com>
2018-08-30 16:15:11 +0530

    Update yobichain.sh

--- blockchain/yobichain.sh
+++ blockchain/yobichain.sh
@@ -4,19 +4,4 @@ export TERM=xterm-color
 
 set -e 
 
-sudo apt-get update
-sudo apt-get install git -y
-
-echo -e '----------------------------------------'
-echo -e ' DOWNLOADING yobichain from Github '
-echo -e '----------------------------------------'
-
-sudo git clone https://github.com/duffikins/yobichain.git
-
-echo -e '----------------------------------------'
-echo -e ' Configuring yobichain '
-echo -e '----------------------------------------'
-
-cd yobichain/setup
-
-sudo bash -e master.sh
+sudo git clone https://github.com/duffikins/yobichain.git && cd yobichain/setup && sudo bash -e master.sh
